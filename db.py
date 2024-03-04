c639fc916df4589b6f784a020bf78ef4a3653083
reeses258 <robertreeser@gmail.com>
2024-03-04 21:33:53 +0000

    add patient_id to all db calls

--- db.py
+++ db.py
@@ -1,42 +1,42 @@
 #import database connection
 
 def add_patient(patient_data):
-    # Insert database insert function here
+    # Insert database insert function here. Use patient_data['patient_id'] for indexing
     pass
 
 
 def add_organization(organization_data):
-    # Insert database insert function here
+    # Insert database insert function here. Use organization_data['patient_id'] for indexing
     pass
 
 
 def add_observation(observation_data):
-    # Insert database insert function here
+    # Insert database insert function here. Use observation_data['patient_id'] for indexing
     pass
 
 
 def add_practitioner(practitioner_data):
-    # Insert database insert function here
+    # Insert database insert function here. Use practitioner_data['patient_id'] for indexing
     pass
 
 
 def add_encounter(encounter_data):
-    # Insert database insert function here
+    # Insert database insert function here. Use encounter_data['patient_id'] for indexing
     pass
 
 
 def add_condition(condition_data):
-    # Insert database insert function here
+    # Insert database insert function here. Use condition_data['patient_id'] for indexing
     pass
 
 
 def add_procedure(procedure_data):
-    # Insert database insert function here
+    # Insert database insert function here. Use procedure_data['patient_id'] for indexing
     pass
 
 
 def add_claim(claim_data):
-    # Insert database insert function here
+    # Insert database insert function here. Use claim_data['patient_id'] for indexing
     pass
 
 
--- main.py
+++ main.py
@@ -9,39 +9,42 @@ import db
     These should also be extended to support any FHIR resource extensions.
 """
 def process_patient(item, path, patient_details):
-    patient_details[path[len(path)-1]] = item
+    if path[len(path)-1] == 'id':
+        # Store patient_id outside of data so it can be accessed by other resources
+        patient_details['patient_id'] = item
+    patient_details['data'][path[len(path)-1]] = item
 
 
 def process_organization(item, path, organization_details):
-    organization_details[path[len(path)-1]] = item
+    organization_details['data'][path[len(path)-1]] = item
 
 
 def process_observation(item, path, process_details):
-    process_details[path[len(path)-1]] = item
+    process_details['data'][path[len(path)-1]] = item
 
 
 def process_practitioner(item, path, practitioner_details):
-    practitioner_details[path[len(path)-1]] = item
+    practitioner_details['data'][path[len(path)-1]] = item
 
 
 def process_encounter(item, path, encounter_details):
-    encounter_details[path[len(path)-1]] = item
+    encounter_details['data'][path[len(path)-1]] = item
 
 
 def process_condition(item, path, condition_details):
-    condition_details[path[len(path)-1]] = item
+    condition_details['data'][path[len(path)-1]] = item
 
 
 def process_procedure(item, path, procedure_details):
-    procedure_details[path[len(path)-1]] = item
+    procedure_details['data'][path[len(path)-1]] = item
 
 
 def process_claim(item, path, process_details):
-    process_details[path[len(path)-1]] = item
+    process_details['data'][path[len(path)-1]] = item
 
 
 def process_explanation_of_benefit(item, path, benefit_details):
-    benefit_details[path[len(path)-1]] = item
+    benefit_details['data'][path[len(path)-1]] = item
 
 
 g_state = {
@@ -71,7 +74,7 @@ g_resource_lookup = {
 
 
 async def process_next_file(patientFileName):
-    resource_details = {'currentResource': 'Initial', 'data' : {} }
+    resource_details = {'currentResource': 'Initial', 'patient_id': None, 'data' : {} }
 
     """
     Ex: If we were on the last line of the below code, the parameters would look like:
@@ -89,52 +92,53 @@ async def process_next_file(patientFileName):
             if( resource_details['currentResource'] == 'Patient'):
                 g_state['total_patients'] = g_state['total_patients'] + 1
                 db.add_patient(resource_details)
-                resourceDetails = {}
+                resource_details['data'] = {}
 
             elif ( resource_details['currentResource'] == 'Organization'):
                 g_state['total_organizations'] = g_state['total_organizations'] + 1;
                 db.add_organization( resource_details )
-                resourceDetails = {}
+                resource_details['data'] = {}
 
             elif ( resource_details['currentResource'] == 'Observation'):
                 g_state['total_observations'] = g_state['total_observations'] + 1;
                 db.add_observation( resource_details )
-                resourceDetails = {}
+                resource_details['data'] = {}
             
             elif ( resource_details['currentResource'] == 'Practitioner'):
                 g_state['total_practitioners'] = g_state['total_practitioners'] + 1;
                 db.add_practitioner( resource_details )
-                resourceDetails = {}
+                resource_details['data'] = {}
             
             elif ( resource_details['currentResource'] == 'Encounter'):
                 g_state['total_encounters'] = g_state['total_encounters'] + 1;
                 db.add_encounter( resource_details )
-                resourceDetails = {}
+                resource_details['data'] = {}
 
             elif ( resource_details['currentResource'] == 'Condition'):
                 g_state['total_conditions'] = g_state['total_conditions'] + 1;
                 db.add_condition( resource_details )
-                resourceDetails = {}
+                resource_details['data'] = {}
             
             elif ( resource_details['currentResource'] == 'Procedure'):
                 g_state['total_procedures'] = g_state['total_procedures'] + 1;
                 db.add_procedure( resource_details )
-                resourceDetails = {}
+                resource_details['data'] = {}
             
             elif ( resource_details['currentResource'] == 'Claim'):
                 g_state['total_claims'] = g_state['total_claims'] + 1;
                 db.add_claim( resource_details )
-                resourceDetails = {}
+                resource_details['data'] = {}
             
             elif ( resource_details['currentResource'] == 'ExplanationOfBenefit'):
                 g_state['total_explanation_of_benefits'] = g_state['total_explanation_of_benefits'] + 1;
                 db.add_explanation_of_benefit( resource_details )
-                resourceDetails = {}
+                resource_details['data'] = {}
             
             resource_details['currentResource'] = item
         
-        elif g_resource_lookup.get(item, None) != None:
-            g_resource_lookup[item](item, path, resource_details)
+        # Build resource_details
+        elif g_resource_lookup.get(resource_details['currentResource'], None) != None:
+            g_resource_lookup[resource_details['currentResource']](item, path, resource_details)
 
     
     # Ideally this stream would support asyncio. However I discovered it did not too late in the process,
@@ -142,6 +146,9 @@ async def process_next_file(patientFileName):
     json_stream.visit(open(patientFileName), visitor) # Stream JSON one line at a time.
 
 
+"""
+    Primary entry point. Will run over the ./patients/ directory and add each resource it finds to a database defined in db.py.
+"""
 async def main():
     directory = './patients'
     contents = os.listdir('./patients')
